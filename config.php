4febd8042787ce2de65c6fb68ae6a758115d90b4
Tjark Saul <tjark@saul.li>
2012-06-29 21:22:00 +0200

    Konfigurationsdatei erstellt

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Konfigurationsdatei fuer Tippspiel-Software
+ */
+
+define('TIPPSPIEL_CONF_HOST', 'localhost');
+define('TIPPSPIEL_CONF_USER', 'tippspiel');
+define('TIPPSPIEL_CONF_PAWD', 'passwort');
+define('TIPPSPIEL_CONF_DABA', 'tippspiel');
