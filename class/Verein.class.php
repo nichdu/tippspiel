b2cbbea7240a08a2693e87f42c43aeb40a5eb782
Tjark Saul <tjark@saul.li>
2012-06-29 21:56:45 +0200

    Verein als Klasse implementiert

---
+++ class/Verein.class.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Diese Klasse repraesentiert einen Verein
+ * @author Tjark Saul <tjark@saul.li>
+ * @copyright Copyright (c) 2012 Tjark Saul. All rights reserved.
+ */
+class Verein
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $kuerzel;
+    /**
+     * @var string
+     */
+    private $logo;
+
+    /**
+     * Erstellt ein neues Objekt dieser Klasse
+     * @param $id int
+     */
+    public function __construct($id)
+    {
+        assert('is_int($id); // $id muss ein Integer sein.');
+        $this->id = $id;
+        $this->loadFromDatabase();
+    }
+
+    private function loadFromDatabase()
+    {
+        $db = Database::getDbObject();
+        $stmt = $db->prepare("SELECT * FROM `vereine` WHERE `id` = ?;");
+        $stmt->bind_param('i', $this->id);
+        if (!$stmt->execute())
+        {
+            throw new mysqli_sql_exception($stmt->error, $stmt->errno);
+        }
+        $stmt->bind_result($this->id, $this->name, $this->kuerzel, $this->logo);
+        if (!$stmt->fetch())
+        {
+            throw new VereinExistiertNichtException();
+        }
+        $stmt->close();
+    }
+
+    /**
+     * Gibt den Namen des Vereins zurueck
+     * @return string der Name des Vereins
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Gibt die ID des Vereins zurueck
+     * @return int die ID des Vereins
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gibt das drei-Buchstaben-Kuerzel des Vereins zurueck
+     * @return string das Kuerzel des Vereins
+     */
+    public function getKuerzel()
+    {
+        return $this->kuerzel;
+    }
+
+    /**
+     * Gibt das Logo des Vereins als Binärdokument zurück
+     * @return string das Logo des Vereins
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+}
