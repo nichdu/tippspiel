c278318f345d0a2a2a1950b27d542b8e744fc7ae
Tjark Saul <tjark@saul.li>
2012-07-03 12:12:41 +0200

    Konfiguration fuer Punktewerte fuer richtige Tipps eingefuegt

--- config.php
+++ config.php
@@ -7,3 +7,7 @@ define('TIPPSPIEL_CONF_HOST', 'localhost');
 define('TIPPSPIEL_CONF_USER', 'tippspiel');
 define('TIPPSPIEL_CONF_PAWD', 'passwort');
 define('TIPPSPIEL_CONF_DABA', 'tippspiel');
+
+define('TIPPSPIEL_CONF_ERGEBNIS_KORREKT', 4);
+define('TIPPSPIEL_CONF_DIFFERENZ_KORREKT', 3);
+define('TIPPSPIEL_CONF_SIEGER_KORREKT', 2);
