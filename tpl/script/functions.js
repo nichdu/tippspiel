b6235c1c5af6b43459608d1c4d7aa6b33243e1e8
Tjark Saul <tjark@saul.li>
2012-07-06 15:15:30 +0200

    Funktionen-JS hinzugefuegt

---
+++ tpl/script/functions.js
@@ -0,0 +1,9 @@
+/**
+ * Javascript-Funktionen
+ * Copyright (c) 2012 Tjark Saul
+ * All rights reserved
+ */
+
+function is_int(value){
+    return ((parseFloat(value) == parseInt(value)) && !isNaN(value));
+}
