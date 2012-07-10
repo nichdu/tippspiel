6a6c8be05737c04f15662bfee8a2de4a24fba3a6
Tjark Saul <tjark@saul.li>
2012-07-10 22:41:50 +0200

    Funktion fuer Mailentschluesselung hinzugefuegt

--- tpl/script/functions.js
+++ tpl/script/functions.js
@@ -7,3 +7,17 @@
 function is_int(value){
     return ((parseFloat(value) == parseInt(value)) && !isNaN(value));
 }
+
+function UnCryptMailto(s, shift) {
+    var n=0;
+    var r="";
+    for(var i=0;i<s.length;i++) {
+        n=s.charCodeAt(i);
+        if (n>=8364) {n = 128;}
+        r += String.fromCharCode(n-(shift));
+    }
+    return r;
+}
+function linkTo_UnCryptMailto(s, shift)	{
+    location.href=UnCryptMailto(s, shift);
+}
